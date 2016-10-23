24d98067dea0ee29052a8f3ec6222b1bb3becf8e
rossinyolks <rossinyolks@gmail.com>
2016-10-23 14:40:22 +0300

    Update app.py

--- app.py
+++ app.py
@@ -30,11 +30,11 @@ def webhook():
 def makeWebhookResult(req):
     #if req.get("result").get("action") != "shipping.cost":
      #   return {}
-    result = req.get("result")
-    parameters = result.get("parameters")
-    zone = parameters.get("shipping-zone")
+   # result = req.get("result")
+   # parameters = result.get("parameters")
+   # zone = parameters.get("shipping-zone")
 
-    cost = {'Europe':100, 'North America':200, 'South America':300, 'Asia':400, 'Africa':500}
+   # cost = {'Europe':100, 'North America':200, 'South America':300, 'Asia':400, 'Africa':500}
 
     speech = "aaa"
 
